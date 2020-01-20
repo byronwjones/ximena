3abcb5d88861386cd70abc838d026c303635336e
Byron Jones <byron@byronwjones.com>
2020-01-19 20:40:23 -0500

    Create global defaults and main settings objects

---
+++ Ximena/Ximena.Configuration/GlobalDefaults.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ximena.Configuration
+{
+    public sealed class GlobalDefaults
+    {
+        internal GlobalDefaults() { }
+
+        public ViewModelSettingDefaults viewModel { get; set; } =
+            new ViewModelSettingDefaults();
+        public NamespaceSettingDefaults nameSpace { get; set; } =
+            new NamespaceSettingDefaults();
+    }
+}
---
+++ Ximena/Ximena.Configuration/RenderSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ximena.Configuration
+{
+    public sealed class RenderSettings
+    {
+        internal RenderSettings() { }
+
+        public string sourceAssembly { get; set; }
+        public string destRoot { get; set; }
+        public GlobalDefaults globalDefaults { get; set; } = new GlobalDefaults();
+        public Dictionary<string, NamespaceSettings> namespaces { get; set; }
+         = new Dictionary<string, NamespaceSettings>();
+    }
+}
